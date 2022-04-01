1a9cc887117ca42d8a7cead69defb014463f9187
Mitch Trigg <mitrigg@davidson.edu>
2022-04-01 01:07:21 -0400

    Chapter 8 commit

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-
+    public GameObject Bullet;
+    public float BulletSpeed = 100f;
+    private bool _isShooting;
+    public float DistanceToGround = .1f;
+    public LayerMask GroundLayer;
+    private CapsuleCollider _col;
+    public float JumpVelocity = 5f;
+    private bool _isJumping;
     public float MoveSpeed = 10f;
     public float RotateSpeed = 75f;
 
@@ -14,23 +21,45 @@ public class PlayerBehavior : MonoBehaviour
 
     void Start(){
         _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        _isShooting |= Input.GetMouseButtonDown(0);
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
-        /*
+        
         this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
         this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
-        */
+        _isJumping |= Input.GetKeyDown(KeyCode.Space);
+        
     }
     void FixedUpdate(){
+        if(_isShooting){
+            GameObject newBullet = Instantiate(Bullet, this.transform.position + new Vector3(1,0,0), this.transform.rotation);
+            Rigidbody BulletRB = newBullet.GetComponent<Rigidbody>();
+            BulletRB.velocity = this.transform.forward * BulletSpeed;
+        }
+        _isShooting = false;
+        if(IsGrounded() && _isJumping){
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+        if(_isJumping){
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+        
+        _isJumping = false;
         Vector3 rotation = Vector3.up * _hInput;
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
         _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+    private bool IsGrounded(){
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
+        return grounded;
     }
 }
